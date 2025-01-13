6e1667e405fe6ce332faf2f5053729a3531c35fa
egemenmermer <egemenmermer@gmail.com>
2025-01-13 12:16:33 +0100

    Update

--- src/main/java/com/ego/casino/security/CustomUserDetails.java
+++ src/main/java/com/ego/casino/security/CustomUserDetails.java
@@ -4,9 +4,12 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.Collection;
 
+@Service
 public class CustomUserDetails implements UserDetails {
 
     private String email;
